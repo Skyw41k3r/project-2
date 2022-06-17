2b2442968b0e87bdca592441f88c43922db1b5a9
Cjnorcro <cjnorcro@gmail.com>
2022-06-16 18:43:50 -0700

    recommiting changes

--- server.js
+++ server.js
@@ -8,14 +8,11 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-
 const hbs = exphbs.create({ helpers });
 
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-
     app.listen(PORT, () => console.log('Now listening'));
 });
 
