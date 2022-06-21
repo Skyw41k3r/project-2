22bc9363b0729452658aeec20f570ddb739d028a
Cjnorcro <cjnorcro@gmail.com>
2022-06-21 14:02:58 -0700

    trying to clean up merge

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -9,12 +9,7 @@ const jokesData = require('./jokes.json')
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-    const users = await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
-    });
-
-   const joke = await jokes.bulkCreate(jokeData,{
+    await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
