5d08ec3fd72f7280ef3e72124c92b9689594fc73
Kevin <kldpd22@gmail.com>
2022-06-20 22:09:43 -0700

    correcting seeds.js to seed jokes too

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,12 +1,19 @@
 const sequelize = require('../config/connection');
 const  User  = require('../models/User');
+const jokes = require('../models/jokes');
 
 const userData = require('./userData.json');
+const jokesData = require('./jokes.json')
 
 const seedDatabase = async() => {
     await sequelize.sync({ force: true });
 
     await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await jokes.bulkCreate(jokesData, {
         individualHooks: true,
         returning: true,
     });
